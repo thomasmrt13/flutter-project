ea15c5ef67a1e7a55f084ac317a09d059079db36
paulmondon <mondon.paul@epitech.eu>
2023-11-29 22:39:02 +0100

    get article

--- tekhub/lib/Firebase/actions/auth_article.dart
+++ tekhub/lib/Firebase/actions/auth_article.dart
@@ -23,7 +23,8 @@ class ArticleService {
       return Result(true, articles); // Return successful result with articles
     } catch (e) {
       print('Error getting articles: $e');
-      return Result(false, 'Error getting articles'); // Return failure result with error message
+      return Result(false,
+          'Error getting articles'); // Return failure result with error message
     }
   }
 
@@ -37,6 +38,38 @@ class ArticleService {
         return ArticleType.tablet;
       default:
         return ArticleType.phone; // Default to phone if type is not recognized
+    }
+  }
+
+  Future<Result> getArticleById(String articleId) async {
+    try {
+      DocumentSnapshot<Map<String, dynamic>> articleSnapshot =
+          await FirebaseFirestore.instance
+              .collection('articles')
+              .doc(articleId)
+              .get();
+
+      if (!articleSnapshot.exists) {
+        return Result(false,
+            'Article not found'); // Return failure result with error message
+      }
+
+      Map<String, dynamic> data =
+          articleSnapshot.data() as Map<String, dynamic>;
+      Article article = Article(
+        id: articleSnapshot.id,
+        name: data['name'] ?? '',
+        price: (data['price'] ?? 0.0).toDouble(),
+        description: data['description'] ?? '',
+        type: _parseArticleType(data['type']),
+        imageUrl: data['imageUrl'] ?? '',
+      );
+
+      return Result(true, article); // Return successful result with the article
+    } catch (e) {
+      print('Error getting article: $e');
+      return Result(false,
+          'Error getting article'); // Return failure result with error message
     }
   }
 }
