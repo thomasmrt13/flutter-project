5c5759d95df58c4a88fa27e82adce2f861ae86af
EntoineH <antoine.hoareau@epitech.eu>
2023-12-01 11:38:44 +0100

    update

--- tekhub/lib/routes/routes.dart
+++ tekhub/lib/routes/routes.dart
@@ -4,7 +4,6 @@ import 'package:tekhub/screens/forget_password_screen.dart';
 import 'package:tekhub/screens/home_screen.dart';
 import 'package:tekhub/screens/login_screen.dart';
 import 'package:tekhub/screens/register_screen.dart';
-import 'package:tekhub/screens/search_screen_page.dart';
 import 'package:tekhub/screens/splash_screen.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
@@ -14,7 +13,6 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'login': (BuildContext contex) => Login(),
     'register': (BuildContext contex) => Register(),
     'forget-password': (BuildContext contex) => ForgetPassword(),
-    'search-screen': (BuildContext contex) => const SearchScreen(),
     'cart': (BuildContext contex) => const Cart(),
   };
 }
